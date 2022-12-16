dfe917d044489bef9b47f31733aec049e5b24043
ConeyIsland <56121645+joshuajcone@users.noreply.github.com>
2022-12-16 07:50:11 -0600

    Update Reboot-Cluster.ps1
    Updated formatting and added more comments.

--- Reboot-Cluster.ps1
+++ Reboot-Cluster.ps1
@@ -15,107 +15,116 @@ $ESXiServers = @(get-cluster $ClusterName | get-vmhost)
 $ScriptTimer = [System.Diagnostics.Stopwatch]::StartNew()
 
 function RebootESXiServer ($CurrentServer) {
-# Variables
-$timeout = New-TimeSpan -Minutes 8
-$2ndtimeout = New-TimeSpan -Minutes 40
-$ServerName = $CurrentServer.Name
+    # Variables
+    $timeout = New-TimeSpan -Minutes 8
+    $2ndtimeout = New-TimeSpan -Minutes 40
+    $ServerName = $CurrentServer.Name
 
-# Write Output of Host being rebooted
-Write-Output “## Rebooting $ServerName ##”
+    # Write Output of Host being rebooted
+    Write-Output “## Rebooting $ServerName ##”
 
-# Get the server state
-$ServerState = (get-vmhost $ServerName).ConnectionState
-
-# If the server was not in MM, then it sets the host for MM
-if ($ServerState -ne “Maintenance”){
-    Write-Output “$ServerName is entering Maintenance Mode”
-    Set-VMhost $CurrentServer -State maintenance -Evacuate | Out-Null
-        
-    # Get the Server State again to check for a server that did not enter MM
+    # Get the server state
     $ServerState = (get-vmhost $ServerName).ConnectionState
 
-    # If server did not enter maintenance mode the script will exit
+    # If the server was not in MM, then it sets the host for MM
     if ($ServerState -ne “Maintenance”) {
+        Write-Output “$ServerName is entering Maintenance Mode”
+        Set-VMhost $CurrentServer -State maintenance -Evacuate | Out-Null
+        
+        # Get the Server State again to check for a server that did not enter MM
+        $ServerState = (get-vmhost $ServerName).ConnectionState
+
+        # If server did not enter maintenance mode the script will exit
+        if ($ServerState -ne “Maintenance”) {
             Write-Output “Server did not enter maintanenace mode. Cancelling remaining servers”
         
-        # Stop the Stopwatch
-        if ($ScriptTimer.IsRunning -eq "True") {$ScriptTimer.Stop()}
+            # Stop the Stopwatch
+            if ($ScriptTimer.IsRunning -eq "True") { $ScriptTimer.Stop() }
         
-        # Disconnect vCenter
+            # Disconnect vCenter
         (Disconnect-VIServer -Server $vCenterServer -Confirm:$false)
-        Exit
-    } # Close check that exits out of the script if server does not enter Maintenance Mode
+            Exit
+        } # Close check that exits out of the script if server does not enter Maintenance Mode
 
-    # Write Ouput the host is in MM
-    Write-Output “$ServerName is in Maintenance Mode”
-} # Close set Maintenance Mode
+        # Write Ouput the host is in MM
+        Write-Output “$ServerName is in Maintenance Mode”
+    } # Close set Maintenance Mode
     
-# If the server was already in MM, then continue to reboot
-elseif ($ServerState -eq “Maintenance”) {
+    # If the server was already in MM, then report, and continue to reboot
+    elseif ($ServerState -eq “Maintenance”) {
         
-    # Write Output if the host was already in MM before being set
-    Write-Output “$ServerName is already in Maintenance Mode”
-} # Close catch if server was already in MM.
+        # Write Output if the host was already in MM before being set
+        Write-Output “$ServerName is already in Maintenance Mode”
+    } # Close catch if server was already in MM.
 
-# Reboot server
-Write-Output “$ServerName is Rebooting”
-Restart-VMHost $CurrentServer -Confirm:$false | Out-Null
+    # Reboot server
+    Write-Output “$ServerName is Rebooting”
+    Restart-VMHost $CurrentServer -Confirm:$false | Out-Null
 
-# Start the Timer
-$RebootTimer = [System.Diagnostics.Stopwatch]::StartNew()
+    # Start the Timer
+    $RebootTimer = [System.Diagnostics.Stopwatch]::StartNew()
 
-# Wait for Server to show as down
-do {
-    Start-Sleep 1
-    $ServerState = (get-vmhost $ServerName).ConnectionState
-} # Close check for server state every second
-until (($ServerState -eq "NotResponding") -or ($RebootTimer.Elapsed -ge $timeout)) 
+    # Check every second for server to show as down
+    do {
+        Start-Sleep 1
+        $ServerState = (get-vmhost $ServerName).ConnectionState
+    } # Close check for server state every second
+
+    # A timeout was added here in case the server reboots without reporting "Not Responding status", behavior introduced in 7.0u3h
+    until (($ServerState -eq "NotResponding") -or ($RebootTimer.Elapsed -ge $timeout)) 
     Write-Output “$ServerName is Down”
     
-# Wait for server to be in maintenance mode
-do {
-Start-Sleep 60
-$ServerState = (get-vmhost $ServerName).ConnectionState
-Write-Output "$ServerName is Waiting for Reboot"
-} # Close waiting for server to reboot
+    # Check every minute for server to be back from reboot and in maintenance mode
+    do {
+        Start-Sleep 60
+        $ServerState = (get-vmhost $ServerName).ConnectionState
+        Write-Output "$ServerName is Waiting for Reboot"
+    } # Close waiting for server to reboot
 
-# Wait for server to come back in Maintenance Mode, or passes $2ndtimeout
-until (($ServerState -eq "Maintenance") -or ($RebootTimer.Elapsed -ge $2ndtimeout)) 
+    # Wait for server to come back in Maintenance Mode, or passes $2ndtimeout
+    until (($ServerState -eq "Maintenance") -or ($RebootTimer.Elapsed -ge $2ndtimeout)) 
 
-# If passed $2ndtimeout the job exits
+    # If passed $2ndtimeout the job exits
     if ($RebootTimer.Elapsed -ge $2ndtimeout) {
             
         # Stop the Stopwatch
-        if ($RebootTimer.IsRunning -eq "True") {$RebootTimer.Stop()}
-        if ($ScriptTimer.IsRunning -eq "True") {$ScriptTimer.Stop()}
+        if ($RebootTimer.IsRunning -eq "True") { $RebootTimer.Stop() }
+        if ($ScriptTimer.IsRunning -eq "True") { $ScriptTimer.Stop() }
+
+        # Inform user which server did not come back online after a reboot
+        Write-Output "$ServerName did not complete reboot"
 
         # Prepare for exit by disconnecting vCenter
-        Write-Output "$ServerName did not complete reboot"
         Disconnect-VIServer -Server $vCenterServer -Confirm:$false | Out-Null
 
         # Exit out
         Exit
     } # Close check for script timeout
+
+    # If the server successfully comes back after reboot, the script continues
     else {
+
+        # Report the total server reboot time.
         $Minutes = $RebootTimer.Elapsed.Minutes
         $Seconds = $RebootTimer.Elapsed.Seconds
         Write-Output "$ServerName is back up. Took $Minutes minutes and $Seconds seconds"
         
         # Stop the Stopwatch
-        if ($RebootTimer.IsRunning -eq "True") {$RebootTimer.Stop()}
-    } # Close output for Maintenace Mode time
+        if ($RebootTimer.IsRunning -eq "True") { $RebootTimer.Stop() }
+    } # Close output for Reboot time
       
-# Exit maintenance mode
-Write-Output "Exiting Maintenance mode"
-Set-VMhost $CurrentServer -State Connected | Out-Null
-do {
-    Start-Sleep 10
-    $ServerState = (get-vmhost $ServerName).ConnectionState
-} # Close check for server to be online and in Maintenance Mode
-while ($ServerState -eq "Maintenance") 
+    # Exit maintenance mode
+    Write-Output "Exiting Maintenance mode"
+    Set-VMhost $CurrentServer -State Connected | Out-Null
+    do {
+        Start-Sleep 10
+        $ServerState = (get-vmhost $ServerName).ConnectionState
+    } # Close check for server to be online and in Maintenance Mode
+    while ($ServerState -eq "Maintenance") 
     Write-Output "## Reboot Complete ##"
 } # Close Reboot Function
 
+# Main loop for the "RebootESXiServer" function.
 foreach ($ESXiServer in $ESXiServers) {
     RebootESXiServer ($ESXiServer)
 }
@@ -126,8 +135,8 @@ $ScriptSeconds = $ScriptTimer.Elapsed.Seconds
 Write-Output "The script is complete. Total time was $ScriptMinutes minutes and $ScriptSeconds seconds"
 
 # Confirm all stopwatches are stopped
-if ($RebootTimer.IsRunning -eq "True") {$RebootTimer.Stop()}
-if ($ScriptTimer.IsRunning -eq "True") {$ScriptTimer.Stop()}
+if ($RebootTimer.IsRunning -eq "True") { $RebootTimer.Stop() }
+if ($ScriptTimer.IsRunning -eq "True") { $ScriptTimer.Stop() }
 
 # Close vCenter connection
 Disconnect-VIServer -Server $vCenterServer -Confirm:$false | Out-Null
