a59e5e81553827a960881625124e5a75f6c7a4ce
ConeyIsland <56121645+joshuajcone@users.noreply.github.com>
2022-12-16 08:39:11 -0600

    Update EPIC-VM-Home.ps1
    fixed formatting

--- EPIC-VM-Home.ps1
+++ EPIC-VM-Home.ps1
@@ -1,4 +1,5 @@
 #Assign tag on VM that matches the home ESXi hostname
+#This will only alert if the VM tag does not match the hostname it lives on.
 
 $vCenterServer = "Your vCenter Name"
 $cluster_name = "Your Cluster Name"
@@ -11,11 +12,10 @@ $Smtp = "SMTP-Relay.Sterilized.com"
 Connect-VIServer -Server $vCenterServer | Out-Null
 
 #Get The VM's in the Cluster
-$VMs = Get-Cluster $cluster_name | Get-VM | Where-Object {$_.PowerState -eq 'PoweredOn'}
+$VMs = Get-Cluster $cluster_name | Get-VM | Where-Object { $_.PowerState -eq 'PoweredOn' }
 
 #Loop for VM's
-foreach($VM in $VMs)
-   {
+foreach ($VM in $VMs) {
     #Get the ESXi hostname of the VM
     $esxHost = Get-VMHost -VM $VM
 
@@ -32,9 +32,9 @@ foreach($VM in $VMs)
         $MailString = "VM $VM Does not have a tag assigned, it currently lives on $esxHost."
         Send-MailMessage -From $From -To $sendTo -Subject "EPICODB-VM $VM No Tag Assigned" -SmtpServer $Smtp -Body $MailString
    
-   }
-   #Check to see if the assigned VMTag matches the ESXi hostname
-   Elseif ($VMTag -notlike $esxHost) {
+    }
+    #Check to see if the assigned VMTag matches the ESXi hostname
+    Elseif ($VMTag -notlike $esxHost) {
 
         #Uncomment the next line to output the VMname in the wrong location to local console
         #Write-Output "$VM needs to move to $VMTag"
@@ -42,7 +42,7 @@ foreach($VM in $VMs)
         #Output the VMname in the wrong location to email.
         $MailString = "VM $VM is on the wrong host, it needs to move to $VMTag."
         Send-MailMessage -From $From -To $sendTo -Subject "EPICODB-VM $VM Not Home" -SmtpServer $Smtp -Body $MailString
-   } 
+    } 
 } # Close foreach loop
 
 # Close vCenter connection
